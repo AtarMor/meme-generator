f51e9b59c42d154be3acfca787a7437c8a3766f2
Atar Mor <atarmor92@gmail.com>
2024-03-02 18:13:25 +0200

    Share API test

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -239,3 +239,22 @@ function doUploadImg(imgDataUrl, onSuccess) {
     XHR.open('POST', '//ca-upload.com/here/upload.php')
     XHR.send(formData)
 }
+
+const shareData = {
+    title: "Meme Editor",
+    text: "Hi",
+    url: window.location.href
+  };
+  
+  const btn = document.querySelector(".test");
+  const resultPara = document.querySelector(".result");
+  
+  // Share must be triggered by "user activation"
+  btn.addEventListener("click", async () => {
+    try {
+      await navigator.share(shareData);
+      resultPara.textContent = "MDN shared successfully";
+    } catch (err) {
+      resultPara.textContent = `Error: ${err}`;
+    }
+  });
